5bf9da097b5f1bdd93185e1d78f3334abb7b7ef9
dylikmax <dylikmax899@gmail.com>
2024-05-16 18:54:11 +0300

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,31 @@
+const burger = document.querySelector(".burger");
+const sideMenu = document.querySelector(".side-menu");
+let isActive = false;
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  sideMenu.classList.toggle("active");
+  if (isActive) {
+    burger_icon.src = "img/burger_icon.svg";
+    overlay.classList.remove("visible");
+  } else {
+    burger_icon.src = "img/cross_icon.svg";
+    overlay.classList.toggle("visible");
+  }
+  isActive = !isActive;
+});
+
+sideMenu.querySelector(".side-menu__container").addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.nodeName === "A") {
+    burger.classList.remove("active");
+    sideMenu.classList.remove("active");
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 700) {
+    burger.classList.remove("active");
+    sideMenu.classList.remove("active");
+  }
+});
